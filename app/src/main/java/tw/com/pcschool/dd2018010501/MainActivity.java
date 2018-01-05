68d50bf3d0b3b99d7e6cf01935e385194149ea47
Teacher <shaojung@gmail.com>
2018-01-05 09:43:25 +0800

    Dialog with button

--- app/src/main/java/tw/com/pcschool/dd2018010501/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010501/MainActivity.java
@@ -1,9 +1,11 @@
 package tw.com.pcschool.dd2018010501;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("This is title");
         builder.setMessage("Hello");
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "按下了確定", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "按下了取消", Toast.LENGTH_SHORT).show();
+            }
+        });
         builder.show();
     }
 }
