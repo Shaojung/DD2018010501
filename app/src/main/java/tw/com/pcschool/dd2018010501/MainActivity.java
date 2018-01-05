a80801cbc74391112583f855506065ecee4ac71a
Teacher <shaojung@gmail.com>
2018-01-05 13:51:40 +0800

    Dialog with edit

--- app/src/main/java/tw/com/pcschool/dd2018010501/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010501/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -150,4 +151,28 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.show();
     }
+    public void click6(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is title");
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        View v1 = inflater.inflate(R.layout.layout1, null);
+
+        builder.setView(v1);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Toast.makeText(MainActivity.this, "按下了確定", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
+    }
+
 }
